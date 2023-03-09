954500317724abebecc1f9f363de3370825e4578
vanessaBonito <vanebonito@hotmail.com>
2023-03-09 16:03:46 -0500

    Add routes for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :appointments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  authenticated :user do
+    root to: "home#dashboard", as: :authenticated_root
+  end
 
   # Defines the root path route ("/")
-  # root "articles#index"
   root to: "home#index"
 end
