76c0260222addceda42a0f403bfcb980ff238e21
vanessaBonito <vanebonito@hotmail.com>
2023-03-09 11:31:52 -0500

    Add validations and relationships to model Appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,7 @@
 class Appointment < ApplicationRecord
-    
+    belongs_to :user
+
+    validates :title, :description, :start_time, :end_time, :user_id, precense: true
+    validates_associated :users
+    validates :end_time, comparison: { greater_than: :start_time }
 end
