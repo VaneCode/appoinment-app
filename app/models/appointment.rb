bda2360bf2c8cd43abb1434152851e0f3e5b4d1e
vanessaBonito <vanebonito@hotmail.com>
2023-03-09 12:50:52 -0500

    Add unit test for model Appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment < ApplicationRecord
     belongs_to :user
 
-    validates :title, :description, :start_time, :end_time, :user_id, precense: true
-    validates_associated :users
+    validates :title, :description, :start_time, :end_time, :user_id, presence: true
+    validates_associated :user
     validates :end_time, comparison: { greater_than: :start_time }
 end
